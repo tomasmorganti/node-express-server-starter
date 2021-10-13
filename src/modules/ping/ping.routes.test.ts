ee6ec2a810cda39ae12877b8afcb3c8a4b0b792a
Tomas Morganti <tomas.ian.morganti@gmail.com>
2021-10-12 21:17:49 -0700

    fix env vars for CI

--- src/modules/ping/ping.routes.test.ts
+++ src/modules/ping/ping.routes.test.ts
@@ -29,8 +29,6 @@ describe('Testing ping routes', () => {
     });
     describe('[POST] /pong bad request missing "message" parameter', () => {
         it('responds with statusCode 400', async () => {
-            console.log('pg ', process.env.PG_CONNECTION_STRING);
-            console.log('sg_key ', process.env.SENDGRID_API_KEY);
             await request(app).post('/pong').expect(400);
         });
     });
